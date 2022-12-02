3a9e0c17ac1ef54a8c36d07e6a2ed0cc2194c9fa
Chris Dunphy <chris@megaparsec.ca>
2022-12-01 21:27:14 -0700

    Got module 5 code working

--- app/app.ts
+++ app/app.ts
@@ -1,57 +1,21 @@
-function startGame() {
-  // Starting a new game
+/// <reference path="player.ts" />
+/// <reference path="game.ts" />
 
-  let playerName: string | undefined = getInputValue("playername");
-  logPlayer(playerName);
+let newGame: Game;
 
-  postScore(100, playerName);
-}
+// add click handler to the start game button
+document.getElementById("startGame")!.addEventListener("click", () => {
+  const player: Player = new Player();
+  player.name = Utility.getInputValue("playername");
 
-function logPlayer(name: string = "MultiMath Player"): void {
-  console.log(`New game starting for player: ${name}`);
-}
+  const problemCount: number = Number(Utility.getInputValue("problemCount"));
+  const factor: number = Number(Utility.getInputValue("factor"));
 
-function getInputValue(elementID: string): string | undefined {
-  const inputElement: HTMLInputElement = <HTMLInputElement>(
-    document.getElementById(elementID)
-  );
-  if (inputElement.value === "") {
-    return undefined;
-  } else {
-    return inputElement.value;
-  }
-}
+  newGame = new Game(player, problemCount, factor);
+  newGame.displayGame();
+});
 
-function postScore(score: number, playerName?: string): void {
-  let logger: (value: string) => void;
-  if (score < 0) {
-    logger = logError;
-  } else {
-    logger = logMessage;
-  }
-  const scoreElement: HTMLElement | null =
-    document.getElementById("postedScores");
-  scoreElement!.innerText = `${score} - ${playerName}`;
-
-  logger(`Score: ${score}`);
-}
-
-document.getElementById("startGame")!.addEventListener("click", startGame);
-
-const logMessage = (message: string) => console.log(message);
-
-function logError(err: string): void {
-  console.error(err);
-}
-
-interface Employee {
-  name: string;
-  title: string;
-}
-
-interface Manager extends Employee {
-  department: string;
-  numOfEmployees: number;
-
-  scheduleMeeting: (topic: string) => void;
-}
+// add click handler to the calculate score button
+document.getElementById("calculate")!.addEventListener("click", () => {
+  newGame.calculateScore();
+});
